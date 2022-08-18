4e0c386991d0db3a28538456c4b7979f850c3a56
protimsaha <protimsaha750@gmail.com>
2022-08-18 13:59:22 +0600

    some changes

--- src/components/useEmployees.js
+++ src/components/useEmployees.js
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 
 const useEmployees = () => {
     const [employeData, setEmployeeData] = useState([])
+    console.log(employeData)
 
     useEffect(() => {
         fetch('https://opensheet.elk.sh/1gH5Kle-styszcHF2G0H8l1w1nDt1RhO9NHNCpHhKK0M/employees')
